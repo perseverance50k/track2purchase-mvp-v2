f38fd7f41992c32c55f68d9083a33849f3a776b4
Serhii Chernikov <55809302+perseverance50k@users.noreply.github.com>
2024-06-06 19:48:24 +0300

    Configured import sorting with ESLint

--- mobile/.eslintrc.js
+++ mobile/.eslintrc.js
@@ -1,4 +1,44 @@
 module.exports = {
   root: true,
+  plugins: ['eslint-plugin-import'],
   extends: '@react-native',
+  rules: {
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: true,
+        ignoreDeclarationSort: true,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+        allowSeparatedGroups: true,
+      },
+    ],
+    'import/no-unresolved': 'error',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'sibling',
+          'parent',
+          'index',
+          'unknown',
+        ],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project: './tsconfig.json',
+      },
+    },
+  },
 };
